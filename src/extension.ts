ec8d9f355ef6075b55b1dadc0530268862c02406
danielbasedow <daniel.basedow@gmail.com>
2016-10-30 12:23:41 +0100

    switch to lein plugin

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,62 @@
 'use strict';
 
 import {
+	CancellationToken,
 	Disposable,
 	ExtensionContext,
+	TextDocumentContentProvider,
+	Uri,
+	window as Window,
+	workspace
 } from 'vscode';
+
+import {
+		Message
+} from 'vscode-jsonrpc';
 
 import {
 	LanguageClient,
  	LanguageClientOptions,
-	Executable
+	Executable,
+	ErrorHandler,
+	ErrorAction,
+	CloseAction,
 } from 'vscode-languageclient';
+
+class ErrorLogger implements ErrorHandler {
+	public error(error: Error, message: Message, count): ErrorAction {
+		console.error(message, error);
+		return ErrorAction.Shutdown;
+	}
+	public closed(): CloseAction {
+		Window.showErrorMessage('The server crashed');
+		return CloseAction.DoNotRestart;
+	}
+}
+
+class JarTextDocumentContentProvider implements TextDocumentContentProvider {
+	public provideTextDocumentContent(uri: Uri, token: CancellationToken) {
+		return "";
+	}
+}
 
 export function activate(ctx: ExtensionContext) {
 	let serverExecutable: Executable = {
 		command: 'lein',
-		args: ['run'],
+		args: ['lein-vscode-server'],
 		options: {
-			cwd: ctx.asAbsolutePath('server'),
+			cwd: workspace.rootPath
 		}
 	};
 
 	let clientOptions: LanguageClientOptions = {
 		documentSelector: ['clojure'],
+		errorHandler: new ErrorLogger(),
 	}
 
 	let disposable = new LanguageClient('Clojure Server', serverExecutable, clientOptions).start();
 
 	ctx.subscriptions.push(disposable);
+
+	workspace.registerTextDocumentContentProvider('jar', new JarTextDocumentContentProvider());
 }
